2dd44be9c08a19686fc020007036f51a1ed668cd
Matheus Maia <mathmferreira@hotmail.com>
2023-09-14 11:35:49 -0300

    test: added unit tests for crud service

---
+++ src/test/java/com/example/gamerating/service/CrudServiceTests.java
@@ -0,0 +1,200 @@
+package com.example.gamerating.service;
+
+import com.example.gamerating.common.UpdaterExample;
+import com.example.gamerating.domain.model.AbstractEntity;
+import com.example.gamerating.repository.EntityRepository;
+import com.example.gamerating.util.ValidationUtils;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings("unchecked")
+public abstract class CrudServiceTests<T extends AbstractEntity> {
+
+    protected abstract EntityRepository<T> getRepository();
+
+    protected abstract CrudService<T> getService();
+
+    protected abstract void updateEntity();
+
+    protected T entityToCreate;
+    protected T expected;
+    protected T newExpected;
+    protected T emptyObject;
+    protected T filterToFound;
+    protected T filterToNotFound;
+    protected T filterEntity1;
+    protected T filterEntity2;
+    protected T toUpdate;
+    protected T toPartialUpdate;
+    protected T partialUpdated;
+
+    @Test
+    public void givenEntity_whenCreate_thenReturnCreatedEntity() {
+        when(getRepository().save(any())).thenReturn(expected);
+
+        T actual = getService().create(entityToCreate);
+
+        assertNotNull(actual);
+        assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void givenEntityWithId_whenCreate_thenThrowEntityExistsException() {
+        when(getRepository().findById(anyLong())).thenReturn(Optional.ofNullable(expected));
+        CrudService<T> service = getService();
+        assertThrows(EntityExistsException.class, () -> service.create(expected));
+        verify(getRepository(), never()).save(any());
+    }
+
+    @Test
+    public void givenEntity_whenFindById_thenReturnEntity() {
+        when(getRepository().findById(anyLong())).thenReturn(Optional.ofNullable(expected));
+
+        Optional<T> actual = getService().findById(1L);
+
+        assertTrue(actual.isPresent());
+        assertEquals(actual.get().toString(), expected.toString());
+    }
+
+    @Test
+    public void givenEntity_whenFindById_thenReturnEmpty() {
+        when(getRepository().findById(anyLong())).thenReturn(Optional.empty());
+
+        Optional<T> actual = getService().findById(2L);
+
+        assertFalse(actual.isPresent());
+    }
+
+    @Test
+    public void givenEntity_whenFindAllWithNoFilters_thenReturnPagedAllEntities() {
+        BeanUtils.copyProperties(expected, newExpected);
+        newExpected.setId(2L);
+
+        Page<T> expectedPage1 = new PageImpl<>(List.of(expected));
+        Pageable page1 = PageRequest.of(0,1);
+        Page<T> expectedPage2 = new PageImpl<>(List.of(newExpected));
+        Pageable page2 = PageRequest.of(1,1);
+
+        when(getRepository().findAll(any(Example.class), eq(page1))).thenReturn(expectedPage1);
+        when(getRepository().findAll(any(Example.class), eq(page2))).thenReturn(expectedPage2);
+
+        Page<T> actual1 = getService().findAll(emptyObject, page1);
+        Page<T> actual2 = getService().findAll(emptyObject, page2);
+
+        assertFalse(actual1.isEmpty());
+        assertFalse(actual2.isEmpty());
+        assertEquals(1, actual1.getContent().size());
+        assertEquals(1, actual2.getContent().size());
+        assertArrayEquals(expectedPage1.getContent().toArray(), actual1.getContent().toArray());
+        assertArrayEquals(expectedPage2.getContent().toArray(), actual2.getContent().toArray());
+    }
+
+    @Test
+    public void givenEntity_whenFindAllWithAnyFilters_thenReturnPagedAllFilteredEntities() {
+        BeanUtils.copyProperties(expected, newExpected);
+        newExpected.setId(2L);
+
+        Page<T> expectedPage1 = new PageImpl<>(List.of(expected));
+        Pageable page1 = PageRequest.of(0,1);
+        Page<T> expectedPage2 = new PageImpl<>(List.of(newExpected));
+        Pageable page2 = PageRequest.of(1,1);
+
+        when(getRepository().findAll(any(Example.class), eq(page1))).thenReturn(expectedPage1);
+        when(getRepository().findAll(any(Example.class), eq(page2))).thenReturn(expectedPage2);
+
+        Page<T> actual1 = getService().findAll(filterEntity1, page1);
+        Page<T> actual2 = getService().findAll(filterEntity2, page2);
+
+        assertFalse(actual1.isEmpty());
+        assertFalse(actual2.isEmpty());
+        assertEquals(1, actual1.getContent().size());
+        assertEquals(1, actual2.getContent().size());
+        assertArrayEquals(expectedPage1.getContent().toArray(), actual1.getContent().toArray());
+        assertArrayEquals(expectedPage2.getContent().toArray(), actual2.getContent().toArray());
+    }
+
+    @Test
+    public void givenEntity_whenFindAllWithNotFoundFilters_thenReturnPagedEmpty() {
+        when(getRepository().findAll(any(Example.class), any(Pageable.class))).thenReturn(Page.empty());
+
+        Page<T> actual = getService().findAll(filterToNotFound, Pageable.unpaged());
+
+        assertTrue(actual.isEmpty());
+    }
+
+    @Test
+    public void givenEntity_whenUpdate_thenReturnUpdatedEntity() {
+        BeanUtils.copyProperties(expected, newExpected);
+        BeanUtils.copyProperties(newExpected, toUpdate);
+        updateEntity();
+
+        when(getRepository().findById(anyLong())).thenReturn(Optional.ofNullable(expected));
+        when(getRepository().save(any())).thenReturn(newExpected);
+
+        T actual = assertDoesNotThrow(() -> getService().update(expected.getId(), toUpdate));
+        assertNotNull(actual);
+        assertEquals(newExpected.toString(), actual.toString());
+    }
+
+    @Test
+    public void givenEntity_whenUpdate_thenThrowEntityNotFoundException() {
+        BeanUtils.copyProperties(expected, newExpected);
+        BeanUtils.copyProperties(newExpected, toUpdate);
+        updateEntity();
+
+        when(getRepository().findById(0L)).thenReturn(Optional.empty());
+        CrudService<T> service = getService();
+        assertThrows(EntityNotFoundException.class, () -> service.update(0L, toUpdate));
+        verify(getRepository(), never()).save(any());
+    }
+
+    @Test
+    public void givenEntity_whenPartialUpdate_thenReturnUpdatedEntity() {
+        when(getService().findById(anyLong())).thenReturn(Optional.of(expected));
+        when(getRepository().save(any())).thenReturn(partialUpdated);
+
+        String[] ignoredProperties = ValidationUtils.getNullPropertyNames(toPartialUpdate);
+        T actual = assertDoesNotThrow(() -> getService().update(1L, new UpdaterExample<>(toPartialUpdate, ignoredProperties)));
+
+        assertNotNull(actual);
+        assertEquals(actual.toString(), partialUpdated.toString());
+    }
+
+    @Test
+    public void givenEntity_whenPartialUpdate_theThrowEntityNotFoundException() {
+        when(getService().findById(anyLong())).thenReturn(Optional.empty());
+
+        String[] ignoredProperties = ValidationUtils.getNullPropertyNames(toPartialUpdate);
+        CrudService<T> service = getService();
+        UpdaterExample<T> example =  new UpdaterExample<>(toPartialUpdate, ignoredProperties);
+
+        assertThrows(EntityNotFoundException.class, () -> service.update(0L, example));
+        verify(getRepository(), never()).save(any());
+    }
+
+    @Test
+    public void givenEntity_whenDelete_thenDeleteEntity() {
+        doNothing().when(getRepository()).deleteById(anyLong());
+        when(getRepository().findById(anyLong())).thenReturn(Optional.ofNullable(expected));
+        assertDoesNotThrow(() -> getService().deleteById(expected.getId()));
+    }
+
+    @Test
+    public void givenEntity_whenDeleteById_thenThrowEntityNotFoundException() {
+        when(getRepository().findById(anyLong())).thenReturn(Optional.empty());
+        CrudService<T> service = getService();
+        assertThrows(EntityNotFoundException.class, () -> service.deleteById(0L));
+        verify(getRepository(), never()).deleteById(anyLong());
+    }
+
+}
