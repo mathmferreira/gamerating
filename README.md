e5c1d62335de2724bfddf484825b951be8ebdbe1
Matheus Maia <mathmferreira@hotmail.com>
2023-10-03 20:13:40 -0300

    docs: updated documentation of swagger and README

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ![Gradle](https://img.shields.io/badge/Gradle-02303A.svg?style=for-the-badge&logo=Gradle&logoColor=white)
 ![Swagger](https://img.shields.io/badge/-Swagger-%23Clojure?style=for-the-badge&logo=swagger&logoColor=white)
 
-## About the project
+## 📋 About the project
 
 This is an API for rating Games and give your opinion about them. The project was built with the purpose of showing my development skills, in order to add to my project portfolio.
 Here you will see the concepts of SOLID, DRY and Clean Code applied, such as generics and polymorphism even in the unit and integration tests. Feel free to take a look at the code.
@@ -19,7 +19,7 @@ Here you will see the concepts of SOLID, DRY and Clean Code applied, such as gen
 - View all the games that you rated
 - Manage your user
 
-## Built With
+## 🔨 Built With
 
 Built in Spring Boot 3 with Spring Security 6, this stateless API uses an encrypted JWT for authentication and JUnit 5 for unit testing purposes, 
 such as RestAssuredMockMvc for integration tests.
@@ -38,27 +38,27 @@ such as RestAssuredMockMvc for integration tests.
 - MockMvc
 - OpenAPI 3 (Swagger)
 
-## Getting Started
+## 🚀 Getting Started
 
 Before getting started, make sure that you followed the prerequisites instructions section.
 
 **For Developers**: you can run this project on your IDE. 
 Just make sure that Java 17, Gradle and Docker Desktop are properly configured.
 
-### Prerequisites
+### ⚙️ Prerequisites
 
 You'll must have Java 17 installed and the JAVA_HOME path correctly configured in the environment variables of your machine.
 You can download it [here](https://adoptium.net/temurin/releases/).
 
 Check if everything is ok by opening your terminal and typing the command below:
 
-```
+```bash
 java -version
 ```
 
 It will return on the console something like this:
 
-```
+```bash
 java version "17.0.8" 2023-07-18 LTS
 Java(TM) SE Runtime Environment (build 17.0.8+9-LTS-211)
 Java HotSpot(TM) 64-Bit Server VM (build 17.0.8+9-LTS-211, mixed mode, sharing)
@@ -69,13 +69,13 @@ Follow the instructions in the official Gradle documentation [here](https://grad
 
 Check if everything is ok by opening your terminal and typing the command below:
 
-```
+```bash
 gradle -v
 ```
 
 It will return on the console the installed version, for example:
 
-```
+```bash
 ------------------------------------------------------------
 Gradle 8.3
 ------------------------------------------------------------
@@ -86,17 +86,17 @@ You can access the docker documentation for installation and configuration instr
 
 After the installation, make sure that your Docker is running.
 
-### Running the application
+### 🏃‍♂️ Running the application
 
 Once you have every prerequisite installed and configured properly, open your terminal and go to the project root folder and follow to the folder bellow: 
 
-```
+```bash
 src > main > java > com > example > gamerating
 ```
 
 You can run the application by executing the command below in the folder mentioned:
 
-```
+```bash
 ./gradlew bootRun
 ```
 
@@ -106,7 +106,7 @@ The application will be started, and the resources will be available at:
 http:/localhost:8080
 ```
 
-### API Docs
+### 📃 API Docs
 
 Once you have the application running, you can access the documentation of the API in the URL:
 
--- src/main/resources/db/migration/V1__initial_database.sql
+++ src/main/resources/db/migration/V1__initial_database.sql
@@ -5,7 +5,7 @@ CREATE SEQUENCE  IF NOT EXISTS game_id_sequence START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE  IF NOT EXISTS rating_id_sequence START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE user_tb (
-   user_id BIGINT NOT NULL,
+   user_id BIGINT NOT NULL DEFAULT NEXTVAL('user_id_sequence'),
    name VARCHAR(255) NOT NULL,
    email VARCHAR(255) NOT NULL,
    pass VARCHAR(255) NOT NULL,
@@ -17,10 +17,10 @@ CREATE TABLE user_tb (
 );
 
 CREATE TABLE game_tb (
-   game_id BIGINT NOT NULL,
+   game_id BIGINT NOT NULL DEFAULT NEXTVAL('game_id_sequence'),
    title VARCHAR(255) NOT NULL,
    description VARCHAR(255),
-   genre_type VARCHAR(255) NOT NULL CHECK (genre_type in ('ACTION','ACTION_ADVENTURE','ADVENTURE','BOARD_CARD_GAME','PUZZLE','ROLE_PLAYING','SANDBOX','SIMULATION','STRATEGY','SPORTS','MMO')),
+   genre_type VARCHAR(255) NOT NULL CHECK (genre_type in ('ACTION','ACTION_ADVENTURE','ADVENTURE','BOARD_CARD_GAME','MMO','MOBA','PUZZLE','ROLE_PLAYING','SANDBOX','SIMULATION','STRATEGY','SPORTS')),
    release_date date NOT NULL,
    aud_created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
    aud_created_by VARCHAR(255) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE game_tb (
 );
 
 CREATE TABLE rating_tb (
-   rating_id BIGINT NOT NULL,
+   rating_id BIGINT NOT NULL DEFAULT NEXTVAL('rating_id_sequence'),
    value INTEGER NOT NULL,
    comments VARCHAR(255),
    user_id BIGINT NOT NULL,
@@ -48,5 +48,17 @@ ALTER TABLE rating_tb ADD CONSTRAINT fk_game_rating FOREIGN KEY (game_id) REFERE
 
 ALTER TABLE rating_tb ADD CONSTRAINT fk_user_rating FOREIGN KEY (user_id) REFERENCES user_tb (user_id);
 
-INSERT INTO user_tb (user_id, name, email, pass, aud_created_date, aud_created_by) VALUES
-    (NEXTVAL('user_id_sequence'), 'Admin', 'admin@gmail.com', '$2a$10$WlNtjqYNbS5PiutTe/resuHNeZ7y5cPDRFrm0Xu7lgmbwfhgatnee', CURRENT_TIMESTAMP, 'system');
+INSERT INTO user_tb (name, email, pass, aud_created_date, aud_created_by) VALUES
+    ('Admin', 'admin@gmail.com', '$2a$10$WlNtjqYNbS5PiutTe/resuHNeZ7y5cPDRFrm0Xu7lgmbwfhgatnee', CURRENT_TIMESTAMP, 'system');
+
+INSERT INTO game_tb (title, description, genre_type, release_date, aud_created_date, aud_created_by) VALUES
+    ('God Of War', 'The game is the eighth installment in the God of War series. This installment is loosely inspired by Norse mythology, with the majority of it set in ancient Scandinavia in the realm of Midgard.', 'ACTION_ADVENTURE', '2018-04-20', '2018-04-20', 'system'),
+    ('Yu-Gi-Oh! Master Duel', 'The game a free-to-play digital collectible card game based on the Yu-Gi-Oh! Trading Card Game, developed and published by Konami.', 'BOARD_CARD_GAME', '2022-01-19', '2022-01-19', 'system'),
+    ('League of Legends', 'Commonly referred to as League, is a multiplayer online battle arena video game developed and published by Riot Games, the game was inspired by Defense of the Ancients, a custom map for Warcraft III.', 'MOBA', '2009-10-27', '2009-10-27', 'system'),
+    ('Devil May Cry 5', 'It is a hack and slash eletronic game developed and published by Capcom. It is the sixth installment overall and the fifth mainline installment in the Devil May Cry series.', 'ACTION_ADVENTURE', '2019-03-08', '2019-03-08', 'system'),
+    ('FIFA 23', 'Is a football video game published by EA Sports. It is the 30th installment in the FIFA series that is developed by EA Sports, the final installment under the FIFA banner.', 'SPORTS', '2022-09-30', '2022-09-30', 'system'),
+    ('Counter-Strike: Global Offensive', 'Is a multiplayer tactical first-person shooter developed by Valve and Hidden Path Entertainment. The game pits two teams, Terrorists and Counter-Terrorists, against each other in different objective-based game modes.', 'ACTION', '2012-08-21', '2012-08-21', 'system'),
+    ('Ori and the Blind Forest', 'Is a video game developed by Moon Studios and published by Microsoft Studios. Players assume control of Ori, a small white spirit, and Sein, the "light and eyes" of the Spirit Tree.', 'ADVENTURE', '2015-03-11', '2015-03-11', 'system'),
+    ('Tricky Towers', 'Is a physics based tower building game puzzle video game that uses a form of the block-stacking problem as the central game mechanic.', 'PUZZLE', '2016-08-02', '2016-08-02', 'system'),
+    ('Perfect World', 'is a 3D adventure and fantasy MMORPG with traditional Chinese settings. Players can take on various roles depending on choice of race and choice of class within that race.', 'MMO', '2005-07-01', '2005-07-01', 'system'),
+    ('The Sims 4', 'Allows players to create and dress characters called "Sims", build and furnish houses for them, and simulate their everyday lives.', 'SIMULATION', '2014-09-02', '2014-09-02', 'system');
